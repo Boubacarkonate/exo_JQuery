7561019f6bbe57f269c0023e15c3fde038176998
Bouba <boubacar.konate@outlook.fr>
2023-11-09 14:14:22 +0100

    premier script en jquery

---
+++ index.js
@@ -0,0 +1,20 @@
+
+/***********************************************
+            initialiser le jQuery : methode 1
+ ***********************************************/
+
+// $(document).ready(function(){
+//     $("#output").html("Welcome to jQuery avec la première méthode !")
+
+// });
+
+
+
+/***************************************************
+            Initialiser le jQuery : methode 2
+ *****************************************************/
+
+
+$(function(){
+    $("#output").html("Welcome to jQuery avec la deuxième méthode !")
+});
